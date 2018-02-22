983efcd01391e5ad7267f952e78fa381c9a4e7d3
五环小胖子 <zer0131@vip.qq.com>
2018-02-22 19:29:04 +0800

    添加glide命令判断

--- pkg.sh
+++ pkg.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if ! command -v glide >/dev/null 2>&1; then
+  echo 'no exists glide'
+  exit 1
+fi
+
 curr_dir=$(cd `dirname $0`; pwd)
 old_gopath=$GOPATH
 export GOPATH=$curr_dir
@@ -9,3 +14,5 @@ cd $curr_dir/src/redisfox
 glide update
 
 export GOPATH=$old_gopath
+
+cd $curr_dir/
