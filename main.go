def8203beba16d5db25138af761a207ab85e3fb5
五环小胖子 <zer0131@vip.qq.com>
2020-07-04 23:34:33 +0800

    配置加载调整，log加载调整，创建上下文，底层函数调用调整

--- main.go
+++ main.go
@@ -2,81 +2,60 @@ package main
 
 import (
 	"RedisFox/conf"
-	"os"
-	"flag"
-	"strconv"
-	"os/signal"
-	"syscall"
-	"sync"
-	"RedisFox/util"
 	"RedisFox/process"
 	"RedisFox/server"
+	"RedisFox/util"
+	"context"
 	"github.com/zer0131/logfox"
 	"log"
+	"os"
+	"os/signal"
+	"strconv"
+	"sync"
+	"syscall"
 )
 
-var cpath string
-var config *conf.Config
-
-//初始化
-func init() {
-	flag.StringVar(&cpath, "config", "./config/redis-fox.yaml", "config path with yml format")
-	flag.Parse()
-	if cpath == "" {
-		log.Fatalf("config path: %s error", cpath)
-		os.Exit(1)
-	}
-	c, err := conf.NewConfig(cpath)
-	if err != nil {
-		log.Fatal(err.Error())
-		os.Exit(1)
-	}
-	config = c
-	err = logfox.Init(config.Logpath, config.Logname, config.Loglevel, config.Logexpire)
-	if err != nil {
-		log.Fatalf("logfox init error")
-		os.Exit(1)
-	}
-	StorePid("")
-}
-
-//存储pid
-func StorePid(path string) {
-	pid := os.Getpid()
-	if len(path) == 0 {
-		path = "./run_redisfox.pid"
-	}
-
-	fout, err := os.OpenFile(path, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0644)
-	if err != nil {
-		os.Exit(1)
-	}
-	defer func() {
-		_ = fout.Close()
-	}()
-	_,_ = fout.WriteString(strconv.Itoa(pid))
-}
+const (
+	configPath = "./config/redis-fox.yaml"
+)
 
 func main() {
+
+	//初始化配置
+	err := conf.NewConfig(configPath)
+	if err != nil {
+		log.Fatal(err.Error())
+		return
+	}
+
+	//上下文
+	ctx := logfox.NewContextWithLogID(context.Background())
+
+	//log初始化
+	err = logfox.Init(conf.ConfigVal.BaseVal.Logpath, conf.ConfigVal.BaseVal.Logname, conf.ConfigVal.BaseVal.Loglevel, conf.ConfigVal.BaseVal.Logexpire)
+	if err != nil {
+		log.Fatalf("logfox init error")
+		return
+	}
 	//日志要最后再关闭
 	defer logfox.Close()
 
 	wg := new(sync.WaitGroup)
 	closeCh := make(chan struct{})
-	probe := util.NewProbe(wg,closeCh)
+	probe := util.NewProbe(wg, closeCh)
 	defer func() {
 		close(closeCh)
 		wg.Wait()
 	}()
 
-	for _,v := range config.Servers {
+	for _, v := range conf.ConfigVal.BaseVal.Servers {
 		processNum := 2
 		srv := v["server"]
 		port, err := strconv.Atoi(v["port"])
 		conntype := v["conntype"]
 		if err != nil {
-			logfox.Error(err.Error())
-			os.Exit(1)
+			logfox.ErrorfWithContext(ctx, "port convert error:%+v", err)
+			return
 		}
 		var password string
 		if v["password"] != "" {
@@ -84,13 +63,13 @@ func main() {
 		}
 
 		//开启redis info存储
-		_,infoErr := process.RunInfo(srv,conntype,password,port,config,probe)
+		_, infoErr := process.RunInfo(ctx, srv, conntype, password, port, probe)
 		if infoErr != nil {
 			processNum--
 		}
 
 		//开启redis monitor
-		_,monitorErr := process.RunMonitor(srv,conntype,password,port,config,probe)
+		_, monitorErr := process.RunMonitor(ctx, srv, conntype, password, port, probe)
 		if monitorErr != nil {
 			processNum--
 		}
@@ -100,14 +79,13 @@ func main() {
 		}
 	}
 
-	//ToDo(zer):缺少wg
-	serv := server.NewServer(config)
+	//ToDo:缺少wg
+	serv := server.NewServer(ctx)
 	defer serv.Stop()
 
 	exitChan := make(chan os.Signal)
 	signal.Notify(exitChan, os.Kill, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGSTOP, syscall.SIGTERM)
 	<-exitChan
 
-	logfox.Info("RedisFox shutdown")
+	logfox.InfoWithContext(ctx, "RedisFox shutdown")
 }
-
