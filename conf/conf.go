cfe3e010826652f02a7ec97d9548c2f745e30d52
五环小胖子 <zer0131@vip.qq.com>
2020-07-04 23:31:46 +0800

    conf结构调整

--- conf/conf.go
+++ conf/conf.go
@@ -1,37 +1,45 @@
 package conf
 
 import (
-	"io/ioutil"
 	"github.com/go-yaml/yaml"
+	"io/ioutil"
 )
 
-type Config struct {
-	Servers  []map[string]string
-	Sleeptime int
-	Maxidle int
-	Maxactive int
+var ConfigVal = &Config{}
+
+type autoBase struct {
+	Servers     []map[string]string
+	Sleeptime   int
+	Maxidle     int
+	Maxactive   int
 	Idletimeout int
-	Datatype string
-	Datapath string
-	Logpath  string
-	Logname  string
-	Loglevel string
-	Logexpire int
-	Serverip string
-	Serverport int
-	Staticdir string
-	Tpldir string
-	Debugmode int
+	Datatype    string
+	Datapath    string
+	Logpath     string
+	Logname     string
+	Loglevel    string
+	Logexpire   int
+	Serverip    string
+	Serverport  int
+	Staticdir   string
+	Tpldir      string
+	Debugmode   int
 }
 
-func NewConfig(path string) (*Config, error) {
+type Config struct {
+	BaseVal autoBase
+	//ToDo: orm
+}
+
+func NewConfig(path string) error {
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	config := new(Config)
-	if err := yaml.Unmarshal([]byte(data), config); err != nil {
-		return nil, err
+	autoBaseVal := autoBase{}
+	if err := yaml.Unmarshal(data, &autoBaseVal); err != nil {
+		return err
 	}
-	return config, nil
+	ConfigVal.BaseVal = autoBaseVal
+	return nil
 }
